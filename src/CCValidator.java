f30ecb4caefaea48a861a83f28d5202a597d7378
Scott Parrish <slparrish@gmail.com>
2025-11-04 05:16:21 -0500

    finished but not as book wants

--- src/CCValidator.java
+++ src/CCValidator.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 // SDEV 200 Module 1 Exercise 6.31
 // CCValidator class a class to implement the Luhn Check algorithm
 // to validate credit card numbers including example code demonstrating
-// its use.
+// its use.  My version converts the number to a string to separate the
+// digits when necessary.  There is probably a way to do this without
+// converting to strings, but I wasn't able to figure it out in the time
+// I allotted to complete the exercise.
 public class CCValidator {
     // isValid accepts card number as a long number and returns the result of the Luhn
     // algorithm (is the total divisible by 10 or not)
@@ -12,11 +15,17 @@ public class CCValidator {
         int sumEven = sumOfDoubleEvenPlace(number); // sum of double even or added digits
         int sumOdd = sumOfOddPlace(number); // sum of the odd places
         int total = sumEven + sumOdd; // add em up
-        System.out.println("sumEven: " + sumEven + " sumOdd: " + sumOdd + " total: " + total);
+
         // intellij had me simplify my if else to this.  It is less readable IMO but in this
         // mess, it's ok.
         return total % 10 == 0;
     }
+
+    // sumOfDoubleEvenPlace() takes the CC number as a long and loops through
+    // the number from right to left.  The numbers in the even places are split
+    // out and sent to getDigit() which returns either the doubled number or
+    // the two digits added together if the doubled number is 10 or larger.
+    // these values are then summed and returned as an int.
     public static int sumOfDoubleEvenPlace(long number) {
         String numberString = Long.toString(number);
         int counter = 0;
@@ -25,9 +34,7 @@ public class CCValidator {
         for (int i = numberString.length() - 1; i >= 0; i--) {
             // only want even numbers from right
             if (counter % 2 != 0) {
-                System.out.println("right number: " + numberString.charAt(i));
                 int numeric = Character.getNumericValue(numberString.charAt(i));
-                System.out.println("getDigit(): " + getDigit(numeric));
                 // get doubled digit and add it to sum
                 sum += getDigit(numeric);
             }
@@ -35,19 +42,28 @@ public class CCValidator {
         }
         return sum;
     }
+
     // getDigit() doubles the number from sumOfDoubleEvenPlace() and
     // returns either the doubled result (number) or the two digits
     // added together if they are greater than 9 (combo)
     public static int getDigit(int number) {
         number = number * 2;
         if (number > 9) {
+            // Convert the number to a string for splitting the digits
             String splitDoubledStr = Integer.toString(number);
+            // splitDoubledStr will be between '10' and '19' combo is the
+            // sum of the numeric values of each digit in the string.
             int combo = Character.getNumericValue(splitDoubledStr.charAt(0)) + Character.getNumericValue(splitDoubledStr.charAt(1));
-            System.out.println("combo " + combo);
             return combo;
         }
         return number;
     }
+
+    // sumOfOddPlace() loops through the CC number from the right
+    // and sums the digits in the odd places, returning the result
+    // as an int.  This method is similar to sumOfDoubledEvenPlace()
+    // with a change at the if statement to take odd places
+    // and minus the call to getDigit()
     public static int sumOfOddPlace(long number) {
         String numberString = Long.toString(number);
         int counter = 0;
@@ -56,9 +72,7 @@ public class CCValidator {
         for (int i = numberString.length() - 1; i >= 0; i--) {
             // only want odd numbers from right
             if (counter % 2 == 0) {
-                System.out.println("odd number: " + numberString.charAt(i));
                 int numeric = Character.getNumericValue(numberString.charAt(i));
-                // System.out.println("getDigit(): " + getDigit(numeric));
                 // get doubled digit and add it to sum
                 sum += numeric;
             }
@@ -66,7 +80,8 @@ public class CCValidator {
         }
         return sum;
     }
-    // I am sure the next 3 methods are supposed to do something but I am
+
+    // I am sure the next 3 methods are supposed to do something, but I am
     // not sure what and my brain is too fried rn to care.
     public static boolean prefixMatched(long number, int d) {
         return true;
